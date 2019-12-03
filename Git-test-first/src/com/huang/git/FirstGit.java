dcb8eb73eed3e1e76439df385bd915aa63c87680
huang <2936817294@qq.com>
2019-12-03 14:42:40 +0800

    idea联合github

---
+++ Git-test-first/src/com/huang/git/FirstGit.java
@@ -0,0 +1,11 @@
+package com.huang.git;
+
+public class FirstGit {
+    public static void main(String[]args){
+        System.out.println("hello,git project");
+        System.out.println("我真的是醉了");
+        System.out.println("hello,github");
+        System.out.println("使用本地版本库修改");
+        System.out.println("自动将文件加入到暂存区");
+    }
+}
